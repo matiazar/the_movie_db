bac3b8d72ca5733b86877f40dc08f931e89f217b
Matias <matias@mazar.com.ar>
2021-03-17 15:04:41 -0300

    cambios en widget carrousel y carpeta movida

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
+import 'package:colorful_safe_area/colorful_safe_area.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+
 import 'package:the_movie_db/src/models/movies_model.dart';
-import 'package:the_movie_db/src/pages/providers/movies_provider.dart';
+import 'package:the_movie_db/src/providers/movies_provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -14,13 +16,15 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   String _posterPath;
 
+  final moviesProvider = new MoviesProvider();
+  // moviesProvider.getPopulars();
+
   @override
   Widget build(BuildContext context) {
-    final moviesProvider = new MoviesProvider();
-    // moviesProvider.getPopulars();
-
     return Scaffold(
       bottomNavigationBar: BottomAppBar(
+        elevation: 0,
+        color: Colors.transparent,
         child: Row(
           children: [
             IconButton(
@@ -38,7 +42,10 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton:
           FloatingActionButton(child: Icon(Icons.search), onPressed: () {}),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      body: SafeArea(
+      body: ColorfulSafeArea(
+        // overflowRules: OverflowRules.all(true),
+        color: Colors.white.withOpacity(0.7),
+        // filter: ImageFilter.blur(sigmaX: 10, sigmaY: 10),
         child: _backgroundApp(context),
       ),
     );
@@ -47,10 +54,13 @@ class _HomePageState extends State<HomePage> {
   _backgroundApp(context) {
     return Container(
       decoration: BoxDecoration(
-        color: Colors.black,
-        image: DecorationImage(
-            image: NetworkImage('https://image.tmdb.org/t/p/w500/$_posterPath'),
-            fit: BoxFit.cover),
+        // color: Colors.black,
+        image: (_posterPath != null && _posterPath != 'null')
+            ? DecorationImage(
+                image: NetworkImage(
+                    'https://image.tmdb.org/t/p/w500/$_posterPath'),
+                fit: BoxFit.cover)
+            : null,
       ),
       width: double.infinity,
       height: double.infinity,
@@ -92,8 +102,6 @@ class _HomePageState extends State<HomePage> {
   }
 
   _listadoPeliculas() {
-    final moviesProvider = new MoviesProvider();
-
     return FutureBuilder(
         future: moviesProvider.getPopulars(),
         builder: (context, AsyncSnapshot<List<Movie>> snapshot) {
@@ -103,45 +111,73 @@ class _HomePageState extends State<HomePage> {
             return Center(child: CircularProgressIndicator());
           }
 
-          // print(snapshot);
+          if (snapshot.data.length > 0)
+            _posterPath = snapshot.data[0].posterPath;
 
-          return PageView.builder(
-            controller: PageController(viewportFraction: 0.8),
+          // print(snapshot);
+          return Swiper(
+            // controller: PageController(viewportFraction: 0.8),
             itemCount: snapshot.data.length,
             itemBuilder: (context, index) =>
                 _verMovie(snapshot.data[index], context),
-            onPageChanged: (index) {
+            // itemCount: 3,
+            viewportFraction: 0.7,
+            scale: 0.6,
+            // pagination: new SwiperPagination(),
+            control: new SwiperControl(),
+            // layout: SwiperLayout.TINDER,
+            // itemHeight: MediaQuery.of(context).size.height * .6,
+            // itemWidth: MediaQuery.of(context).size.height * .7,
+            onIndexChanged: (index) {
               // print(index);
               setState(() {
                 _posterPath = snapshot.data[index].posterPath;
               });
             },
           );
+
+          // return PageView.builder(
+          //   controller: PageController(viewportFraction: 0.8),
+          //   itemCount: snapshot.data.length,
+          //   itemBuilder: (context, index) =>
+          //       _verMovie(snapshot.data[index], context),
+          //   onPageChanged: (index) {
+          //     // print(index);
+          //     setState(() {
+          //       _posterPath = snapshot.data[index].posterPath;
+          //     });
+          //   },
+          // );
         });
   }
 
   _verMovie(Movie movie, context) {
-    return Center(
-      child: Column(
-        children: [
-          Padding(
-            padding:
-                const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
-            child: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: FadeInImage(
-                    height: MediaQuery.of(context).size.height * .6,
-                    fit: BoxFit.cover,
-                    placeholder: AssetImage('assets/loading.gif'),
-                    image: NetworkImage(
-                        'https://image.tmdb.org/t/p/w500/${movie.posterPath}'))),
-          ),
-          Text(
-            movie.title,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-        ],
+    return GestureDetector(
+      onTap: () {
+        print(movie);
+      },
+      child: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 10.0),
+              child: ClipRRect(
+                  borderRadius: BorderRadius.circular(15.0),
+                  child: FadeInImage(
+                      height: MediaQuery.of(context).size.height * .6,
+                      fit: BoxFit.cover,
+                      placeholder: AssetImage('assets/loading.gif'),
+                      image: NetworkImage(
+                          'https://image.tmdb.org/t/p/w500/${movie.posterPath}'))),
+            ),
+            Text(
+              movie.title,
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+          ],
+        ),
       ),
     );
   }
